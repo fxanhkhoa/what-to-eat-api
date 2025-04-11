d5d1d0fcbef768317bd6dab33cc64ea1ec50cdd7
fxanhkhoa <fxanhkhoa@gmail.com>
2025-04-11 22:52:37 +0700

    init

--- go.mod
+++ go.mod
@@ -33,7 +33,12 @@ require (
 	github.com/gookit/color v1.5.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -45,11 +50,11 @@ require (
 	github.com/zishang520/engine.io-go-parser v1.2.3 // indirect
 	github.com/zishang520/socket.io-go-parser v1.0.4 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/oauth2 v0.13.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/appengine/v2 v2.0.2 // indirect
@@ -69,15 +74,16 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v4 v4.13.3
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sosodev/duration v1.2.0 // indirect
 	github.com/urfave/cli/v2 v2.27.1 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	github.com/zishang520/socket.io v1.3.2
-	golang.org/x/mod v0.16.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.19.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,99 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"what-to-eat/be/auth"
-	"what-to-eat/be/controllers"
-	"what-to-eat/be/directive"
-	"what-to-eat/be/firebase"
-	"what-to-eat/be/graph"
-	"what-to-eat/be/shared"
-	socketio_helper "what-to-eat/be/socketio"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
+	"github.com/labstack/echo-contrib/echoprometheus"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	e := godotenv.Load()
-	if e != nil {
-		fmt.Print(e)
-	}
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+	e := echo.New()
 
-	shared.InitializeMongoDB()
-	firebase.InitFirebase()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
 
-	router := mux.NewRouter().StrictSlash(true)
+	e.Pre(middleware.AddTrailingSlash())
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
 
-	dishRouter := router.PathPrefix("/dish").Subrouter()
-	dishRouter.HandleFunc("/", controllers.NewDishController().Find).Methods("GET")
-	dishRouter.HandleFunc("/random", controllers.NewDishController().FindRandom).Methods("GET")
-	dishRouter.HandleFunc("/{slug}", controllers.NewDishController().FindOne).Methods("GET")
+	e.Use(middleware.Logger())
+	e.Use(echoprometheus.NewMiddleware("myapp"))   // adds middleware to gather metrics
+	e.GET("/metrics", echoprometheus.NewHandler()) // adds route to serve gathered metrics
 
-	ingredientRouter := router.PathPrefix("/ingredient").Subrouter()
-	ingredientRouter.HandleFunc("/", controllers.NewIngredientController().Find).Methods("GET")
-	ingredientRouter.HandleFunc("/{slug}", controllers.NewIngredientController().FindOne).Methods("GET")
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
 
-	dishVoteRouter := router.PathPrefix("/dish-vote").Subrouter()
-	dishVoteRouter.HandleFunc("/", controllers.NewDishVoteController().Create).Methods("POST")
-	dishVoteRouter.HandleFunc("/{id}", controllers.NewDishVoteController().Update).Methods("PATCH")
-	dishVoteRouter.HandleFunc("/{id}", controllers.NewDishVoteController().FindOne).Methods("GET")
-
-	authRouter := router.Methods(http.MethodPost, http.MethodGet).Subrouter()
-	authRouter.HandleFunc("/login", auth.NewAuthController().Login)
-	authRouter.HandleFunc("/retrieve-token", auth.NewAuthController().RetrieveToken)
-
-	c := graph.Config{Resolvers: &graph.Resolver{}}
-	c.Directives.HasRole = directive.Role
-
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(c))
-
-	graphRouter := router.Methods(http.MethodGet, http.MethodPost, http.MethodPatch, http.MethodPut, http.MethodDelete).Subrouter()
-	graphRouter.Use(auth.Middleware())
-
-	authRouter.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	graphRouter.Handle("/query", srv)
-
-	headersOk := handlers.AllowedHeaders([]string{"Access-Control-Allow-Origin", "Content-Type", "Authorization"})
-	originsOk := handlers.AllowedOrigins([]string{"*"})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-
-	io := socketio_helper.InitializeSocketIO()
-	router.Handle("/socket.io/", io.ServeHandler(nil))
-	router.Handle("/", http.FileServer(http.Dir("./asset")))
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(headersOk, originsOk, methodsOk)(router)))
-
-	exit := make(chan struct{})
-	SignalC := make(chan os.Signal)
-
-	signal.Notify(SignalC, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	go func() {
-		for s := range SignalC {
-			switch s {
-			case os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
-				close(exit)
-				return
-			}
-		}
-	}()
-
-	<-exit
-	io.Close(nil)
-	os.Exit(0)
+	e.Logger.Fatal(e.Start(":1323"))
 }
