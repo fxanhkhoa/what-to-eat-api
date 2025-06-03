de1b13f53bd2987e9ae55747ec1ee408ab6ef7e6
fxanhkhoa <fxanhkhoa@gmail.com>
2025-06-03 23:41:41 +0700

    update

--- router/dish.go
+++ router/dish.go
@@ -13,6 +13,7 @@ func UseDishRouter(group *echo.Group) {
 	rG := middleware.NewRoleGuard()
 	controller := &controllers.DishController{}
 	group.GET("/", controller.Find)
+	group.GET("/random/", controller.FindRandom)
 	group.GET("/:id/", controller.FindOne)
 	group.GET("/slug/:slug/", controller.FindOneBySlug)
 	group.POST("/", controller.Create, aG.AuthGuard, rG.RoleGuard([]string{constants.CREATE_DISH}))
